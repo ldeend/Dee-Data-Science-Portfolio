8b506976454fc96880f8e95c0078d1767947d5b7
ldeend <ldee@nd.edu>
2026-02-08 23:07:27 -0500

    Added Streamlit app

--- basic_streamlit_app/main.py
+++ basic_streamlit_app/main.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+#title
+st.title("College Basketball Streamlit App")
+
+#short description
+st.write(
+    "This Streamlit app loads a dataset from a CSV file of Division I college basketball teams from games this season so far."
+)
+
+# data
+df = pd.read_csv("basic_streamlit_app/data/cbb25.csv")
 
