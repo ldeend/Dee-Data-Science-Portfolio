5d2f34fcbea1eb2d6aad95008bb3804e86782fd8
Lucas Dee <ldee@nd.edu>
2026-05-07 04:36:58 -0400

    Refactor CSV loading and enhance code formatting
    Updated file path for CSV loading and improved formatting.

--- basic_streamlit_app/main.py
+++ basic_streamlit_app/main.py
@@ -1,16 +1,15 @@
 import streamlit as st
 import pandas as pd
+import os
 
-#title
+# title
 st.title("College Basketball Streamlit App")
-
-#short description
+# short description
 st.write(
     "This Streamlit app loads a dataset from a CSV file of Division I college basketball teams from games this season so far."
 )
-
 # data
-df = pd.read_csv("basic_streamlit_app/data/cbb25.csv")
+df = pd.read_csv(os.path.join(os.path.dirname(__file__), "data", "cbb25.csv"))
 
 st.subheader("All teams, full dataset")
 st.dataframe(df)
@@ -20,34 +19,26 @@ st.dataframe(df)
 st.subheader("Specific Team Data")
 Team = st.selectbox("Select a team", df["Team"].unique())
 team_df = df[df["Team"] == Team]
-
 st.dataframe(team_df)
 
 
 # conference filter
 st.subheader("Filter by Conference")
-
 all_conferences = sorted(df["CONF"].unique())
-
 CONF = st.multiselect(
     "Choose one or multiple conferences",
     all_conferences,
     default=all_conferences)
-
 conf_df = df[df["CONF"].isin(CONF)]
-
 st.dataframe(conf_df)
+
 
 # wins slider filter
 st.subheader("Filter by Wins")
-
 min_wins, max_wins = st.slider("Number of Wins",
-    int(df["W"].min()), 
-    int(df["W"].max()), 
+    int(df["W"].min()),
+    int(df["W"].max()),
     (int(df["W"].min()), int(df["W"].max()))
 )
-
 wins_df = df[(df["W"] >= min_wins) & (df["W"] <= max_wins)]
-
 st.dataframe(wins_df)
-
